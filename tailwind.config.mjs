2755e0888352704c66a79dcec054778a42cd63ac
jisan5620jk <jisank5620@gmail.com>
2025-01-28 17:02:51 +0600

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,45 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        Black: '#161616',
+        mediumBlack: '#212121',
+        normalBlack: '#272727',
+        lightBlack: ' #1e1e1e',
+        slateGray: '',
+        gray: '#616161',
+        lightGray: '#acacac',
+        khaki: '#c19d68',
+        whiteSmoke: '#f8f6f3',
+      },
+      fontFamily: {
+        Garamond: ['Cormorant Garamond', 'serif'],
+        Lora: ['Lora', 'serif'],
+      },
+      screens: {
+        esm: '480px',
+        // sm: "576px",
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        '2xl': '1400px',
+        '3xl': '1600px ',
+        '4xl': '1700px',
+        // 1400-1600, 1300-1399,1200-1299,992-1199(1170),768-991,600-767,480-599,320-479
+      },
+      lineHeight: {},
+      boxShadow: {
+        custom: '0px 5px 15px rgba(204, 204, 204, 0.25)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
